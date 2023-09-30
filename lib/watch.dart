77f7f0dbc5237a7392058928b87be1eacf7dbc72
Deniz <49831545+creeperkafasi@users.noreply.github.com>
2023-09-30 18:43:21 +0300

    Added keyboard support to the player

--- lib/watch.dart
+++ lib/watch.dart
@@ -1,5 +1,6 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:miteru/utils/allanime.dart';
 import 'package:video_player/video_player.dart';
 import 'package:wakelock/wakelock.dart';
@@ -43,69 +44,103 @@ class _WatchPageState extends State<WatchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SafeArea(
-        child: Container(
-            color: Colors.black,
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Builder(builder: (context) {
-              playerController = VideoPlayerController.networkUrl(
-                widget.qualities[selectedQualityIndex].url,
-              );
-              return Chewie(
-                controller: ChewieController(
-                  startAt: savedPosition ?? Duration.zero,
-                  autoPlay: savedPosition != null,
-                  videoPlayerController: playerController!,
-                  aspectRatio: aspectRatio,
-                  autoInitialize: true,
-                  additionalOptions: (context) => [
-                    OptionItem(
-                      onTap: () {
-                        showDialog(
-                            context: context,
-                            builder: ((context) {
-                              return SimpleDialog(
-                                title: const Text("Select Quality:"),
-                                children: [
-                                  Column(
-                                      children: widget.qualities
-                                          .map(
-                                            (e) => Padding(
-                                              padding:
-                                                  const EdgeInsets.all(8.0),
-                                              child: TextButton.icon(
-                                                onPressed: () async {
-                                                  savedPosition =
-                                                      await playerController
-                                                          ?.position;
-                                                  setState(() {
-                                                    playerController?.dispose();
-                                                    selectedQualityIndex =
-                                                        widget.qualities
-                                                            .indexOf(e);
-                                                  });
-                                                },
-                                                icon:
-                                                    const Icon(Icons.settings),
-                                                label: Text(e.name),
+      body: Shortcuts(
+        shortcuts: {
+          LogicalKeySet(LogicalKeyboardKey.space): VoidCallbackIntent(
+            () => playerController?.value.isPlaying ?? false
+                ? playerController?.pause()
+                : playerController?.play(),
+          ),
+          LogicalKeySet(LogicalKeyboardKey.arrowRight): VoidCallbackIntent(
+            () async => playerController?.seekTo(
+              (await playerController!.position)! + const Duration(seconds: 10),
+            ),
+          ),
+          LogicalKeySet(LogicalKeyboardKey.arrowLeft): VoidCallbackIntent(
+            () async => playerController?.seekTo(
+              (await playerController!.position)! - const Duration(seconds: 10),
+            ),
+          ),
+          LogicalKeySet(LogicalKeyboardKey.arrowUp): VoidCallbackIntent(
+            () async => playerController?.setVolume(
+              (playerController!.value.volume + 0.1).clamp(0, 1),
+            ),
+          ),
+          LogicalKeySet(LogicalKeyboardKey.arrowDown): VoidCallbackIntent(
+            () async => playerController?.setVolume(
+              (playerController!.value.volume - 0.1).clamp(0, 1),
+            ),
+          ),
+        },
+        child: Focus(
+          autofocus: true,
+          child: SafeArea(
+            child: Container(
+              color: Colors.black,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Builder(builder: (context) {
+                playerController = VideoPlayerController.networkUrl(
+                  widget.qualities[selectedQualityIndex].url,
+                );
+                return Chewie(
+                  controller: ChewieController(
+                    startAt: savedPosition ?? Duration.zero,
+                    autoPlay: savedPosition != null,
+                    videoPlayerController: playerController!,
+                    aspectRatio: aspectRatio,
+                    autoInitialize: true,
+                    additionalOptions: (context) => [
+                      OptionItem(
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: ((context) {
+                                return SimpleDialog(
+                                  title: const Text("Select Quality:"),
+                                  children: [
+                                    Column(
+                                        children: widget.qualities
+                                            .map(
+                                              (e) => Padding(
+                                                padding:
+                                                    const EdgeInsets.all(8.0),
+                                                child: TextButton.icon(
+                                                  onPressed: () async {
+                                                    savedPosition =
+                                                        await playerController
+                                                            ?.position;
+                                                    setState(() {
+                                                      playerController
+                                                          ?.dispose();
+                                                      selectedQualityIndex =
+                                                          widget.qualities
+                                                              .indexOf(e);
+                                                    });
+                                                  },
+                                                  icon: const Icon(
+                                                      Icons.settings),
+                                                  label: Text(e.name),
+                                                ),
                                               ),
-                                            ),
-                                          )
-                                          .toList()),
-                                ],
-                              );
-                            }));
-                      },
-                      iconData: Icons.high_quality,
-                      title: "Quality",
-                    )
-                  ],
-                  // autoPlay: true,
-                  // fullScreenByDefault: true,
-                ),
-              );
-            })),
+                                            )
+                                            .toList()),
+                                  ],
+                                );
+                              }));
+                        },
+                        iconData: Icons.high_quality,
+                        title: "Quality",
+                      )
+                    ],
+                    // autoPlay: true,
+                    // fullScreenByDefault: true,
+                  ),
+                );
+              }),
+            ),
+          ),
+        ),
       ),
     );
   }
